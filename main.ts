60adaa6f0d2e00dca2ec9eabb35a3a9d2088b939
MJ <ursmanu1@gmail.com>
2023-03-22 15:31:22 -0400

    updated PR activity

--- main.ts
+++ main.ts
@@ -1,268 +1,213 @@
 import axios from 'axios';
 import 'dotenv/config';
-import * as Slack from '@slack/webhook';
-//import * as Slack from 'typed-slack';
+import { WebClient } from "@slack/web-api";
+import csv from 'csv-parser';
+import fs from 'fs';
 
-// Setting Pre-requisites with empty lists
 let msg: any;
 let pr_url_list: string[] = [];
 let pr_reviewer_list: string[] = [];
-let pr_reviewer_tasks: string[] = [];
 let pr_cd_day_list: string[] = [];
 let created_on: any = ' ';
 let reviewer_names: string[] = [];
-let activity_display_name: any;
-let task_id: any;
-let comment_created_on: any = ' ';
-let comment_updated_on: any = ' ';
-let diffCommentData: any = ' ';
-let diffCommentDataRound: any = ' ';
-let username: string = '';
-let BB_WORKSPACE: string ='SOME_WORKSPACE';
-
-
-// Slack API: either use the Webhook URL option from bot or use direct API for POST messages
-let SLACK_WEBHOOK_URL = 'SLACK_WEBHOOK_URL'
+let BB_WORKSPACE: string = 'automation-mj';
+let SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL
 
 function slack_api_call(msg: any) {
-let options = {
-    method: 'POST',
-    url: SLACK_WEBHOOK_URL,  
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    data: {text: msg}
-  };
-  
-  axios.request(options).then(function (response) {
-    console.log(response.data);
-  }).catch(function (error) {
-    console.error(error);
-  });
+    let options = {
+        method: 'POST',
+        url: SLACK_WEBHOOK_URL,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        data: { text: msg }
+    };
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
 }
 
-// function api_call(msg: string | Slack.IncomingWebhookSendArguments){
-//     let slack = new Slack.IncomingWebhook(SLACK_WEBHOOK_URL);
-//     slack.send(msg);
-//     console.log('Message sent: ' + msg);
-//     }
+async function getEmailForUsername(username: string): Promise<string | undefined> {
+    const users = [];
+    await new Promise<void>((resolve, reject) => {
+        fs.createReadStream('input.csv')
+            .pipe(csv())
+            .on('data', (row) => {
+                users.push(row)
+            })
+            .on('error', (error) => reject(error))
+            .on('end', () => resolve());
+    });
+    const user = users.find((u) => u.username === username);
 
-// Get PullReq Created times
+    if (!user) {
+        return undefined;
+    }
+
+    return user.email;
+}
+
 function getPRTimes(created_on: string | number | Date) {
     const currentDate: any = new Date();
-    let newDate:any = new Date(created_on);
+    let newDate: any = new Date(created_on);
 
     let diffTime = currentDate - newDate;
-    let diffDate = diffTime/(1000*3600*24);
+    let diffDate = diffTime / (1000 * 3600 * 24);
     let diffDateRound = Math.round(diffDate)
     //console.log(diffDateRound);
     return diffDateRound;
 
 }
 
-// Get PullReq Activity (comments/tasks) times
-function getCommentTimes(comment_created_on: any, comment_updated_on: any) {
-    comment_created_on = new Date(comment_created_on);
-    comment_updated_on = new Date(comment_updated_on);
+const getSlackUsers = async (reviewer_names) => {
 
-    let diffCommentTime = comment_created_on - comment_updated_on;
-    let diffCommentDate = diffCommentTime/(1000*3600*24);
-    let diffCommentDateRound = Math.round(diffCommentDate)
-    //console.log(diffCommentDateRound);
-    return diffCommentDateRound;
+    const reviewer_email = [];
+    for (var name in reviewer_names) {
+        const email = await getEmailForUsername(reviewer_names[name]);
+        if (email) {
+            reviewer_email.push(email);
+        }
+    }
+    const SLACK_TOKEN =
+        process.env.SLACK_TOKEN;
+    const web = new WebClient(SLACK_TOKEN);
+    try {
+        // Call the users.list method of the WebClient instance to fetch all users
+        const result = await web.users.list();
+        // Return the array of users from the response
+        const data = [];
 
-}
+        result.members.map((member) => {
+            if (member.profile.email && member.name && reviewer_email.includes(member.profile.email)) {
+                data.push(member.id);
+            }
+        });
+        return data;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+};
 
-//msg = '<!here>  yo DEVs ' + '\n';
+const fetchComments = async (apiUrl: string, accessToken: string, reviewerUsernames: any) => {
+    try {
+        var OneDay = new Date().getTime() + (1 * 24 * 60 * 60 * 1000);
+        const response = await axios.get(apiUrl, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+        const comments = response.data.values;
+        const recentComments = comments.filter(
+            (comment) => new Date(comment.created_on).getTime() <= OneDay
+        );
+        const filteredComments = recentComments.filter(
+            (comment) => !reviewerUsernames.includes(comment.user.username)
+        );
+        return filteredComments;
+    } catch (error) {
+        console.error(error);
+    }
+};
 
-// function newContent(reviewer_names) {
-//     msg = '<!here>  yo DEVs ' + '\n';
-//     msg += (size) + ' PR(s) waiting on Repo: ' +  repo + '\n';
-//     msg += (response.data.values[i].title)+ ':: ' + pr_url_list[i] + '\n';
-//     msg += ':fire: It was opened ' + pr_cd_day_list[i] + ' day(s) ago.\n';
-//     msg += 'Reviewers::::: '+ reviewer_names + '\n';
-//     console.log(msg)
-
-// }
-
-
-// Getting Repo List from BitBucket Workspace
-async function getRepos(){
-    const responserepos = await axios.get('https://api.bitbucket.org/2.0/repositories/' + BB_WORKSPACE+ '?limit=100' , {
-    method: "GET",
-    headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env.BB_ACCESS_TOKEN}`
-
-      }
-    })
-    const repolist: string [] = [];
-    responserepos.data.values.forEach((element: any) => {
-        repolist.push(element.slug);
-    })
-    return repolist;
-}
-
- getRepos().then(async (repolist) => {
-    
-
-     //msg = '<!here>  yo DEVs ' + '\n';
-    //console.log("msg text "+ msg);
-    // get repo list and pr size    
-    for (const repo of repolist) {
-        //console.log(repo)
-        const reponsePRStatus = await axios.get('https://api.bitbucket.org/2.0/repositories/' + BB_WORKSPACE+ '/' + repo + '/pullrequests' +'?state=OPEN' , {
+async function getRepos() {
+    try {
+        const responserepos = await axios.get('https://api.bitbucket.org/2.0/repositories/' + BB_WORKSPACE + '?limit=100', {
             method: "GET",
             headers: {
                 'Content-Type': 'application/json',
                 Authorization: `Bearer ${process.env.BB_ACCESS_TOKEN}`
-        
-            }   
+
+            }
         })
- 
-    
-    //}).then(response => {
+        const repolist: string[] = [];
+        responserepos.data.values.forEach((element: any) => {
+            repolist.push(element.slug);
+        })
+        return repolist;
+    } catch (error) {
+        console.log(error);
+        console.log("Unable to get repos");
+        return [];
+    }
+}
+
+getRepos().then(async (repolist) => {
+    for (const repo of repolist) {
+        const reponsePRStatus = await axios.get('https://api.bitbucket.org/2.0/repositories/' + BB_WORKSPACE + '/' + repo + '/pullrequests' + '?state=OPEN', {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${process.env.BB_ACCESS_TOKEN}`
+
+            }
+        })
+
         let size: any = reponsePRStatus.data.values.length;
-        if (size > 0)
-        {
-            //console.log("msg repo "+ msg);
+        if (size > 0) {
             let i: any;
-            //let reviewer_names: any;
-            // Get each repo's PullReqs in the workspace
-            for ( i in reponsePRStatus.data.values) {
-                //console.log(response.data.values[i].title);
-                pr_url_list.push('https://api.bitbucket.org/2.0/repositories/' + BB_WORKSPACE+ '/' + repo + '/pullrequests/' + (reponsePRStatus.data['values'][i]['id']))
-                
-                 //msg += (response.data.values[i].title)+ ':: ' + pr_url_list[i] + '\n'
-                //console.log("msg url "+ msg);
+            for (i in reponsePRStatus.data.values) {
+                pr_url_list.push('https://api.bitbucket.org/2.0/repositories/' + BB_WORKSPACE + '/' + repo + '/pullrequests/' + (reponsePRStatus.data['values'][i]['id']))
                 created_on = (reponsePRStatus.data.values[i].created_on);
                 let dateDiff: any = getPRTimes(created_on);
                 pr_cd_day_list.push(dateDiff);
-                // Get each repo's PullReq active reviewers
-                let pr_reviewer_url = 'https://api.bitbucket.org/2.0/repositories/' + BB_WORKSPACE+ '/' + repo + '/pullrequests/' + (reponsePRStatus.data['values'][i]['id']) + '?fields=reviewers'
-
+                let pr_reviewer_url = 'https://api.bitbucket.org/2.0/repositories/' + BB_WORKSPACE + '/' + repo + '/pullrequests/' + (reponsePRStatus.data['values'][i]['id']) + '?fields=reviewers'
                 let options = {
                     method: 'GET',
                     url: pr_reviewer_url,
                     headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${process.env.BB_ACCESS_TOKEN}`
-                }
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${process.env.BB_ACCESS_TOKEN}`
+                    }
                 };
                 const responseReviewers = await axios.request(options);
                 let count = 0;
                 let j: any;
-                // Get Reviewers display names and add it to list
-                for (j in (responseReviewers.data.reviewers)){
-                    let rev_display_name = (responseReviewers.data.reviewers[j].display_name) ;
-                        if (rev_display_name != null ) {
-                        //console.log(rev_display_name);
+                for (j in (responseReviewers.data.reviewers)) {
+                    let rev_display_name = (responseReviewers.data.reviewers[j].display_name);
+                    if (rev_display_name != null) {
                         count += 1;
-                            pr_reviewer_list.push(rev_display_name)
-                        }
-                        else {
-                            pr_reviewer_list.push(' No Rewiewers')
-                        }
+                        pr_reviewer_list.push(rev_display_name)
+                    }
+                    else {
+                        pr_reviewer_list.push(' No Rewiewers')
+                    }
                 }
-                reviewer_names =  Array.from(new Set(pr_reviewer_list));
-                console.log("List of Reviewers: " + reviewer_names);
+                reviewer_names = Array.from(new Set(pr_reviewer_list));
 
-                // **********
-                // Ignore user when there is an active task opened by them on a specific PullRequest
-                
-                let pr_reviewer_activity_url = 'https://api.bitbucket.org/2.0/repositories/' + BB_WORKSPACE+ '/' + repo + '/pullrequests/' + (reponsePRStatus.data['values'][i]['id']) + '/activity';
-                let pr_reviewer_tasks_url = 'https://api.bitbucket.org/2.0/repositories/' + BB_WORKSPACE+ '/' + repo + '/pullrequests/' + (reponsePRStatus.data['values'][i]['id']) + '/tasks';
-                    
-                    let activity_options = {
-                        method: 'GET',
-                        url: pr_reviewer_activity_url,
-                        headers: {
-                        'Content-Type': 'application/json',
-                        Authorization: `Bearer ${process.env.BB_ACCESS_TOKEN}`
-                        }
-                    };
+                const comments = await fetchComments('https://api.bitbucket.org/2.0/repositories/' + BB_WORKSPACE + '/' + repo + '/pullrequests/' + (reponsePRStatus.data['values'][i]['id']) + '/comments', process.env.BB_ACCESS_TOKEN, reviewer_names);
+                const slackReviewer = await getSlackUsers(reviewer_names);
+                var commenti = 1
+                msg = 'Comments:::::' + '\n';
+                comments.forEach((comment) => {
+                    msg += `${commenti}` + '\n';
+                    msg += `${comment.content.raw}` + '\n';
+                    msg += `Comment By : ${comment.user.display_name}` + '\n';
+                    msg += `PR Title : ${comment.pullrequest.title}` + '\n';
+                    msg += '\n';
+                    msg += '\n';
+                    commenti = commenti + 1;
+                })
 
-                    const reponseActivity = await axios.request(activity_options);
-
-                    let task_options = {
-                        method: 'GET',
-                        url: pr_reviewer_tasks_url,
-                        headers: {
-                        'Content-Type': 'application/json',
-                        Authorization: `Bearer ${process.env.BB_ACCESS_TOKEN}`
-                        }
-                    };
-
-                    const reponseTasks = await axios.request(task_options);
-                    //console.log(reponseTasks.data.values[j]);
-                    //const keys = Object.keys(reponseActivity);
-                    //console.log(pr_reviewer_activity_url);
-                    //console.log("before loop:  " + reviewer_names.length);
-                    let k: any;
-                    //console.log("K: " + k);
-                    // Comments API causing issues
-                    //activity_display_name = (reponseActivity.data.values[j].comment.user.display_name) ;
-                    //console.log(activity_display_name);
-
-                    
-                    //console.log("before loop:  " + reviewer_names.length);
-                    for ( k =0; k < reviewer_names.length; k++){
-                        //console.log("K in loop: " + k);
-                        //console.log("loop start:  " + reviewer_names.length);
-                        //console.log(reponseActivity.data.values[j].comment.id);
-                        //console.log(reponseTasks.data.values[j]);
-                        if (('comment' in reponseActivity.data.values[j]) && 'id' in (reponseTasks.data.values[j])) {
-                       // if (('comment' in reponseActivity.data.values[j])) {
-                            activity_display_name = (reponseActivity.data.values[j].comment.user.display_name) ;
-                            task_id = (reponseTasks.data.values[j]);
-                            //console.log("Comment created by: " + activity_display_name);
-                            if ((activity_display_name !== null && (typeof(activity_display_name) !== undefined) || (activity_display_name !== undefined))
-                                || task_id !== null || ((typeof(task_id) !== undefined) || (task_id !== undefined))) {
-                                comment_created_on = (reponseActivity.data.values[j].comment.created_on);
-                                comment_updated_on = (reponseActivity.data.values[j].comment.updated_on);
-                                //let commentDiff = getCommentTimes(comment_created_on, comment_updated_on);
-                                let commentDiff = getCommentTimes(comment_updated_on, created_on);
-                                //console.log("New Time: " + commentDiff);
-                                //console.log("Comment created by: " + activity_display_name); 
-                                if ( reviewer_names.includes(activity_display_name) && commentDiff <= 1){
-                                    reviewer_names.shift();
-                                    console.log("Remaining Reviewers: " + reviewer_names)
-                                }             
-                            }         
-                        }   
-                    }         
-                
-                    //console.log("Its diffDate: " + getCommentTimes(comment_created_on, comment_updated_on));
-                    //console.log("Its them: 2 " + reviewer_names)
-
-                // **********
-
-                let pr_data_values  = reponsePRStatus.data.values[i];
+                let pr_data_values = reponsePRStatus.data.values[i];
                 //msg = '<!here>  yo DEVs ' + '\n';
-                msg =  'PR # '+ (pr_data_values.id) +' waiting on Repo: ' +  repo + '\n';
-                msg += (pr_data_values.title)+ ':: '  + (pr_data_values.links.html.href) + '\n';
+                msg += 'PR # ' + (pr_data_values.id) + ' waiting on Repo: ' + repo + '\n';
+                msg += (pr_data_values.title) + ':: ' + (pr_data_values.links.html.href) + '\n';
                 msg += ':fire: It was opened ' + pr_cd_day_list[i] + ' day(s) ago.\n';
-                
-                msg += 'Reviewers:::::' + '\n';
-                reviewer_names.forEach(function (each_rev_name){
-                    let username = each_rev_name.replace(" ", "_").toLowerCase();
-                    msg += '<@' + username + '> '
-                });
 
-                
-                console.log(msg);
-                //slack_api_call(msg);
+                msg += 'Reviewers:::::' + '\n';
+                slackReviewer.forEach(function (memberId) {
+                    msg += '<@' + memberId + '> '
+                });
                 console.log("after:  " + reviewer_names.length + '\n');
-            } 
-       
+            }
+            slack_api_call(msg);
         }
-        else 
-        {
+        else {
             console.log("No PRs found");
         }
-    slack_api_call(msg)
-    console.log(msg);
-        
     }
 })
 
--- match-emails.ts
+++ match-emails.ts
@@ -1,32 +1,42 @@
-import axios from 'axios';
-import fs from 'fs';
-import csv2json = require('csvtojson');
-
+import axios from "axios";
+import fs from "fs";
+import csv2json = require("csvtojson");
+import { WebClient } from "@slack/web-api";
+import 'dotenv/config';
 
 let emailList: any[] = [];
 let emailIDs: any[] = [];
 let emailID: any;
 let myNames: string;
 let filtered: any;
-const userList: string [] = [];
-let users: any [];
+const userList: string[] = [];
+let users: any[];
 
-interface User {  name: string; email: string };
+interface User {
+  name: string;
+  email: string;
+}
 
 // convert users.csv file to JSON array
 // Get all users from BitBucket CSV to find their usernames and email addresses
 async function getBitBucketEmails() {
   try {
     // get json output of downloaded CSV list
-     users = await csv2json().fromFile('input.csv');
-     // Rename CSV column names to JSON object keys such as "Public name": name
-     const changeStrings = users.map(({ "Public name": name, "Email address": email, ...rest }) => ({name, email, ...rest}));
-     //console.log(flat);
-     // get array object of BB username and email (needs this format to compare againt slack user details)
-     filtered = changeStrings.map(({ name, email} : User ) => ({ name, email}));
-    //console.log(filtered); 
+    users = await csv2json().fromFile("input.csv");
+    // Rename CSV column names to JSON object keys such as "Public name": name
+    const changeStrings = users.map(
+      ({ "Public name": name, "Email address": email, ...rest }) => ({
+        name,
+        email,
+        ...rest,
+      })
+    );
+    //console.log(flat);
+    // get array object of BB username and email (needs this format to compare againt slack user details)
+    filtered = changeStrings.map(({ name, email }: User) => ({ name, email }));
+    //console.log(filtered);
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
   console.log(filtered);
 
@@ -35,66 +45,90 @@ async function getBitBucketEmails() {
 //getBitBucketEmails();
 
 // Get all users from Slack to find their usernames and email addresses
-async function getSlackEmails() { //: Promise<string[]>{
-    let url = 'https://app.slack.com/api/users.list';
+async function getSlackEmails() {
+  //: Promise<string[]>{
+  let url = "https://app.slack.com/api/users.list";
 
-    let options = {
-        method: 'GET',
-        url: url,
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Bearer ACTUAL_SLACK_TOKEN'
-        }
-    };
-    // const responseProfiles = await axios.request(options).then(function (res) {
-    const responseProfiles =  await axios.request(options);
-    //console.log(responseProfiles.data);
-    let j: any;
-    let count = 0;
-    let target: any = {};
-    for (j in (responseProfiles.data.members)){
-        //console.log(responseProfiles.data.members[j].name);
-        let rev_display_name = (responseProfiles.data.members[j].name) ;
-        let rev_email = (responseProfiles.data.members[j].profile.email) ;
+  let options = {
+    method: "GET",
+    url: url,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Bearer ${process.env.SLACK_TOKEN}`,
+    },
+  };
+  console.log(options);
+  // const responseProfiles = await axios.request(options).then(function (res) {
+  const responseProfiles = await axios.request(options);
+  //console.log(responseProfiles.data);
+  let j: any;
+  let count = 0;
+  let target: any = {};
+  for (j in responseProfiles.data.members) {
+    //console.log(responseProfiles.data.members[j].name);
+    let rev_display_name = responseProfiles.data.members[j].name;
+    let rev_email = responseProfiles.data.members[j].profile.email;
 
-        if (rev_display_name != null && rev_email != null) {
-            //console.log(rev_display_name);
-            count += 1;
-            //userList.push(rev_email)
-           userList.push("{ name: '" + rev_display_name + "' , email: '" + rev_email + "' }");
-           //target = Object.assign({},...userList.map(key => ({[key]: rev_display_name})));             
-        }
-            // else {
-            //     userList.push(' No Rewiewers')
-            // }
+    if (rev_display_name != null && rev_email != null) {
+      //console.log(rev_display_name);
+      count += 1;
+      //userList.push(rev_email)
+      userList.push(
+        "{ name: '" + rev_display_name + "' , email: '" + rev_email + "' }"
+      );
+      //target = Object.assign({},...userList.map(key => ({[key]: rev_display_name})));
     }
-    let updatedList: any = JSON.stringify(userList);
-   //console.log(updatedList.replace(/"/g, ""));
-   //console.log(userList);
-   //updatedList = Object.entries(updatedList).map(([name, email]) => `${name}: ${email}`);
-   //console.log(updatedList);
-    return updatedList;
+    // else {
+    //     userList.push(' No Rewiewers')
+    // }
+  }
+  let updatedList: any = JSON.stringify(userList);
+  //console.log(updatedList.replace(/"/g, ""));
+  //console.log(userList);
+  //updatedList = Object.entries(updatedList).map(([name, email]) => `${name}: ${email}`);
+  //console.log(updatedList);
+  return updatedList;
 }
 //getSlackEmails();
 
 // Find if there is any match between emails and then get their slack username to send @mention on the slack channel.
 // This not a working fucntion
-async function findMatchEmails(){
-    let bitb = await getBitBucketEmails();
-    let slack = await getSlackEmails();
-    
-    for (let i = 0; i < slack.length; i++) {
-        console.log("MeI")
-        for (let j = 0; j < bitb.length; j++) {
-            console.log("MeJ")
-          if (slack[i].name === bitb[j].name) {
-            console.log(slack[i].name);
-          }
-        }
+async function findMatchEmails() {
+  let bitb = await getBitBucketEmails();
+  let slack = await getSlackEmails();
+
+  console.log(slack);
+  for (let i = 0; i < slack.length; i++) {
+    console.log("MeI");
+    for (let j = 0; j < bitb.length; j++) {
+      console.log("MeJ");
+      if (slack[i].name === bitb[j].name) {
+        console.log(slack[i].name);
       }
-   }
+    }
+  }
+}
 
- findMatchEmails();
+// findMatchEmails();
 
+const getSlackUsers = async () => {
+  const SLACK_TOKEN =
+    "xoxb-2087098036294-4977504131303-cV7hMzK0fdjafUULXud3IYOb";
+  const web = new WebClient(SLACK_TOKEN);
 
-
+  try {
+    // Call the users.list method of the WebClient instance to fetch all users
+    const result = await web.users.list();
+    // Return the array of users from the response
+    const emails = [];
+    result.members.map((member) => {
+      if (member.profile.email && member.name) {
+        emails.push({ email: member.profile.email, name: member.name });
+      }
+    });
+    return emails;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
